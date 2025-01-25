e1c26894d1fae379574e49a3cea5726d2bfe6e9b
server <webmaster@oxfordassassins.co.uk>
2025-01-25 11:16:54 +0100

    Slightyl dodgy redirects with nginx

--- assassins-website/src/app/app.routes.ts
+++ assassins-website/src/app/app.routes.ts
@@ -1,10 +1,6 @@
 import { Routes } from '@angular/router';
-import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
-import { DiscordComponent } from './discord/discord.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'signup', component: SignupComponent},
-    {path: 'discord', component: DiscordComponent}
+    {path: '', component: HomeComponent}
 ];
